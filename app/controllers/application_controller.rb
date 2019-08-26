9804c81c62d3155f2eaaa2d3ec0abdb14e81eaf1
13erdprosage <oshumonchan@mac.com>
2019-08-26 19:23:24 +0900

    Basic認証を本番環境でのみ実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   # Basic認証
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
