30d54193c94eec43f52d5c349a759b0e814e0451
13erdprosage <oshumonchan@mac.com>
2019-08-26 15:37:13 +0900

    テーブル、カラムの作成
    * Validates
    * 制約
    * アソシエーション
    * 外部キー
    といった条件も記載。

--- db/migrate/20190826060657_create_products.rb
+++ db/migrate/20190826060657_create_products.rb
@@ -1,6 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.string     :name,          null: false
+      t.integer    :postage,       null: false
+      t.integer    :price,         null: false
+      t.text       :description,   null: false
+      t.string     :status,        null: false
+      t.string     :shipping_date, null: false
+      t.references :address_id,    foreign_key: true
+      t.references :category_id,   foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20190826060707_create_images.rb
+++ db/migrate/20190826060707_create_images.rb
@@ -1,6 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
+      t.string     :image_url,  null: false
+      t.references :product_id, foreign_key: true
 
       t.timestamps
     end
