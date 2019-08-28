59d8f9bb90dfee7c523c0459b91deba4b6a9e8f9
13erdprosage <oshumonchan@mac.com>
2019-08-28 12:03:01 +0900

    Basin認証コード実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,12 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth, if: :production?
-  # protect_from_forgery with: :exception
+  before_action :basic_auth
+  protect_from_forgery with: :exception
 
-  # private
+  private
 
-  # def production?
-  #   Rails.env.production?
-  # end
-
-  # # Basic認証
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #   end
-  # end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
