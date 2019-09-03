4f4cd45515781911b6abda03f273a23840a02c41
kenji <totyu_go2y@yahoo.co.jp>
2019-09-03 15:52:56 +0900

    ルーティング（ページ表示）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  root 'products#index'
-  resources :products, only: :new
+  devise_for :users
+  get 'products/top'
+  get 'products/show'
+  get 'products/new'
+  get 'products/confirmation'
+
+  get 'users/logout'
+  get 'users/mypage'
+  get 'users/details'
+  get 'users/telephone'
+  get 'users/telephonesecond'
+  get 'users/address'
+  get 'users/payment'
+  get 'users/finish'
+  get 'users/sociallink'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/registration'
+
 end
