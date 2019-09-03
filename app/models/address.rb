c63c188961e56b3ca7601cc338abf767c4a99cac
SAWANO10 <key.strawberry.1000@gmail.com>
2019-09-03 18:12:29 +0900

    ユーザー登録機能の実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,11 @@
 class Address < ApplicationRecord
   has_one :product
+  belongs_to :user, optional: true
 
-  validates :postal_code, :city, :address, :prefecture, presence: true
+  validates :postal_code, :city, :address, :prefecture_id, presence: true
+
+  class Address < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :prefecture
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  root 'products#index'
+  devise_for :users 
+    
+  root to: 'products#index'
+  
+  resources :signup do
+    collection do
+      get 'new1'
+      get 'new2'
+      get 'new3'
+      get 'new4' 
+      get 'new5'
+      get 'complete' 
+    end
+  end
+  
+  resources :users, only: [:edit, :update, :show]
+  resources :productss, only: [:edit, :update, :show]
 end
