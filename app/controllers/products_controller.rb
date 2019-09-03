ab225bfe9fa56557a68e372437a5d09a9e864a33
kenji <totyu_go2y@yahoo.co.jp>
2019-09-03 19:39:17 +0900

    コントローラー（修正）

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    redirect_to :action => 'top'
-    redirect_to :action => 'confirmation'
-  end
-
-  def top
-  end
-  
-  def confirmation
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,5 @@
 class UsersController < ApplicationController
-
   def index
-    redirect_to :action => 'details'
-    redirect_to :action => 'telephone'
-    redirect_to :action => 'telephonesecond'
-    redirect_to :action => 'address'
-    redirect_to :action => 'payment'
-    redirect_to :action => 'finish'
-    redirect_to :action => 'sociallink'
-    redirect_to :action => 'registration'
-    redirect_to :action => 'mypage'
-    redirect_to :action => 'logout'
-  end
-
-  def logout
-  end
-  
-  def mypage
-  end
-
-  def details
-  end
-
-  def telephone
-  end
-
-  def telephonesecond
-  end
-
-  def address
-  end
-
-  def payment
-  end
-
-  def finish
-  end
-
-  def sociallink
-  end
-
-  def registration
   end
 
   def show
