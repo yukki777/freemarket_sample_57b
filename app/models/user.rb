f8be07b0e592bd4edf56139acc92994ae6ef29a6
yukki <konan297goo@gmail.com>
2019-09-04 14:07:16 +0900

    新規画面にパス追加
    フェイスブックとグーグル。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   has_many :products
   has_one :address
