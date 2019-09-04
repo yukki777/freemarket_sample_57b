2f8136e98a4209e3078aff372f1ca30759fe4b2f
SAWANO10 <key.strawberry.1000@gmail.com>
2019-09-04 16:25:06 +0900

    テスト用ファイルを追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "is valid with all" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+
+    it "is invalid without a nickname" do
+      user = build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "is invalid without a email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "is invalid without a birthday" do
+      user = build(:user, birthday: nil)
+      user.valid?
+      expect(user.errors[:birthday]).to include("can't be blank")
+    end
+
+    it "is invalid without a first_name" do
+      user = build(:user, first_name: nil)
+      user.valid?
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it "is invalid without a first_name_kana" do
+      user = build(:user, first_name_kana: nil)
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("can't be blank")
+    end
+
+    it "is invalid without a family_name" do
+      user = build(:user, family_name: nil)
+      user.valid?
+      expect(user.errors[:family_name]).to include("can't be blank")
+    end
+
+    it "is invalid without a birthday" do
+      user = build(:user, family_name_kana: nil)
+      user.valid?
+      expect(user.errors[:family_name_kana]).to include("can't be blank")
+    end
+
+    it "is invalid without a password" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "is invalid without a password_confirmation although with a password" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "is valid with a password that has less than 7 characters " do
+      user = build(:user, password: "0000000", password_confirmation: "0000000")
+      expect(user).to be_valid
+    end
+
+    it "is invalid with a password that has less than 6 characters " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user.errors[:password][0]).to include("is too short")
+    end
+   
+  end
+end
