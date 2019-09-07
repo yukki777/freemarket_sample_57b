82a29094bfd5359c5a6c27dc6f931f01b05fbf2f
yukki <konan297goo@gmail.com>
2019-09-07 17:10:03 +0900

    自動生成されたコントローラーの削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,15 +12,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     provider = provider.to_sym
-    
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    # @user = User.new
-    # ↑SNSアカウントを持っていない場合の検証用
+
     if @user.persisted?
-      # Active Record object がDB に保存済みかどうかを判定するメソッド です。
-      sign_in_and_redirect @user, event: :authentication 
+      sign_in_and_redirect @user, event: :authentication
+
     else
       unless @user.provider.blank?
+
       session[:uid] = @user.uid
       session[:email] = @user.email
       session[:provider] = @user.provider
@@ -28,39 +27,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session[:password] = @user.password
       session[:first_name] = @user.first_name
       session[:family_name] = @user.family_name
-      # redirect_to new_user_registration_path
       redirect_to new1_signup_index_path
+
       else
         session[:value] = provider
         redirect_to new_user_session_path, notice: "#{provider}での登録情報はありません。他の方法でログインをお試しください。"
       end
     end
   end
-
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
