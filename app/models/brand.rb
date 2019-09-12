6712951735777ca4c07e5d7f98032a2acb377a19
SAWANO10 <key.strawberry.1000@gmail.com>
2019-09-12 14:51:49 +0900

    ブランド情報を追加

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,8 +1,8 @@
 class Brand < ActiveHash::Base
   self.data = [
-    {id: 1, name: 'シャネル'},
-    {id: 2, name: 'ナイキ'},
-    {id: 3, name: 'ルイ ヴィトン'},
-  
-  ]
+   {id: '', name: '---'},
+   {id: 1, name: 'シャネル'}, {id: 2, name: 'ナイキ'},
+   {id: 3, name: 'ルイ ヴィトン'}, {id: 4, name: 'シュプリーム',
+   {id: 5, name: 'アディダス'},
+ ]
 end
