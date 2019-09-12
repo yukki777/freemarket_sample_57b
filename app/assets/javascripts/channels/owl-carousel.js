fd8a79f7abec6383d6b93e7ef41565122b74eaff
SAWANO10 <key.strawberry.1000@gmail.com>
2019-09-12 11:50:07 +0900

    owlcarousel-railsの記述を追加

---
+++ app/assets/javascripts/channels/owl-carousel.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(".feature.owl-carousel").owlCarousel({
+    loop:true,
+    nav:true,
+    items:1,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    smartSpeed: 1000
+  });
+});
