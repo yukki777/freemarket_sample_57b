f9c7d0171cd81b9c7432e8c41a0162e3924cad45
yukki <konan297goo@gmail.com>
2019-09-16 15:50:42 +0900

    ファクトリー作成

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :address, class: Address do
+    postal_code {"123-4567"}
+    city {Faker::Address.city}
+    address {Faker::Address.street_address}
+    building_name {Faker::Dessert.variety}
+    prefecture_id {"1"}
+
+    trait :prefecture do
+      prefecture
+    end
+    # association :prefecture, factory: :prefecture
+    association :user, factory: :user
+
+  end
+end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,16 +1,20 @@
 FactoryBot.define do
-
-  factory :product do
-    name                {"商品名"}
-    price               {"価格"}
-    description         {"こんにちは"}
-    user_id	            {"1"}
-    address_id          {"1"}
+  factory :product, class: Product do
+    name { Faker::Games::Pokemon.name } 
+    price               {"100"}
+    description {Faker::Movies::BackToTheFuture.quote}
     brand_id            {"1"}
     postage_id          {"1"}
     shipping_method_id  {"1"}
     size_id	            {"1"}
     shipping_date_id    {"1"}
     condition_id        {"1"}
+    category_id         {"1"}
+    # binding.pry
+    association :user, factory: :user
+    association :address, factory: :address
+    
+    
+
   end
 end
